820d27797b3d325583dd7a514d8d1dbfb52b7c0f
timliuGH <timsgithub@gmail.com>
2019-08-29 06:35:21 -0700

    Import csv file into books table

--- import.py
+++ import.py
@@ -27,7 +27,7 @@ def create_books_table():
         print("Creating 'books' table..")
         db.execute("CREATE TABLE books (\
             id SERIAL PRIMARY KEY, \
-            isbn INTEGER NOT NULL, \
+            isbn VARCHAR NOT NULL, \
             title VARCHAR NOT NULL, \
             author VARCHAR NOT NULL, \
             year INTEGER NOT NULL\
@@ -37,77 +37,58 @@ def create_books_table():
         print("Table 'books' already exists")
     db.commit()
 
+def insert_books_data():
+    """Insert data from csv into books table"""
+    # Get data from csv file
+    print("Getting data from csv..")
+    file = open("books.csv")
+    reader = csv.reader(file)
 
-# def create_passengers_table():
-#     """Create passengers table if doesn't already exist"""
-#     try:
-#         print("Creating 'passengers' table..")
-#         db.execute("CREATE TABLE passengers (\
-#             id SERIAL PRIMARY KEY, \
-#             name VARCHAR NOT NULL, \
-#             flight_id INTEGER REFERENCES flights\
-#             )")
-#         print("Table 'passengers' created")
-#     except exc.ProgrammingError as err:
-#         print("Table 'passengers' already exists")
-#     db.commit()
-#
-#
-# def insert_flight_data():
-#     """Insert data from csv into flights table"""
-#     # Get data from csv file
-#     print("Getting data from csv..")
-#     file = open("flight-info.csv")
-#     reader = csv.reader(file)
-#
-#     # Insert csv data into table
-#     print("Inserting data into 'flights' table..")
-#     for csv_origin, csv_destination, csv_duration in reader:
-#         db.execute("INSERT INTO flights (origin, destination, duration)\
-#             VALUES (:origin, :destination, :duration)", {
-#             "origin": csv_origin,
-#             "destination": csv_destination,
-#             "duration": csv_duration
-#             })
-#     print("Data inserted")
-#     db.commit()
-#
-#
-# def delete_flights_table():
-#     """Delete flights table"""
-#     try:
-#         print("Attempting to delete table 'flights'")
-#         db.execute("DROP TABLE flights")
-#     except exc.ProgrammingError as err:
-#         print("Table 'flights' does not exist")
-#     else:
-#         print("Deleted table 'flights'")
-#
-#     db.commit()
-#
-#
-# def delete_passengers_table():
-#     """Delete passengers table"""
-#     try:
-#         print("Attempting to delete table 'passengers'")
-#         db.execute("DROP TABLE passengers")
-#     except exc.ProgrammingError as err:
-#         print("Table 'passengers' does not exist")
-#     else:
-#         print("Deleted table 'passengers'")
-#     db.commit()
+    # Insert csv data into table
+    print("Inserting data into 'books' table..")
+    for isbn, title, author, year in reader:
+        try:
+            db.execute("INSERT INTO books (isbn, title, author, year)\
+                VALUES (:isbn, :title, :author, :year)", {
+                "isbn": isbn, "title": title, "author": author, "year": year })
+        except exc.DataError as err:
+            print("Invalid entry in csv file")
+        db.commit()
+    print("Data inserted")
+
+def delete_users_table():
+    """Delete users table"""
+    try:
+        print("Attempting to delete table 'users'")
+        db.execute("DROP TABLE users")
+    except exc.ProgrammingError as err:
+        print("Table 'users' does not exist")
+    else:
+        print("Deleted table 'users'")
+
+    db.commit()
+
+def delete_books_table():
+    """Delete books table"""
+    try:
+        print("Attempting to delete table 'books'")
+        db.execute("DROP TABLE books")
+    except exc.ProgrammingError as err:
+        print("Table 'books' does not exist")
+    else:
+        print("Deleted table 'books'")
+
+    db.commit()
 
 
 def main():
     """
-    delete_passengers_table()
-    delete_flights_table()
-    create_flights_table()
-    create_passengers_table()
-    insert_flight_data()
-    """
+    delete_users_table()
+    delete_books_table()
     create_users_table()
     create_books_table()
+    insert_books_data()
+    """
 
 if __name__ == "__main__":
     main()
